0cfb614313cbaf145c8f98739e5bb6a04f503d1d
JCxYIS (DIYPC-2020) <jsn811@gmail.com>
2020-02-10 20:10:11 +0800

    a

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '首頁\n<a src="index.html">Title</a>' });
 });
+
+app.get('/index.html', function (req, res) {
+  res.sendFile( __dirname + "/views/index.html" );
+})
 
 module.exports = router;
