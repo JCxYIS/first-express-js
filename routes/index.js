d2d3eec4d5c25b50c63c4c5e44fdd8b5067ef708
JCxYIS (DIYPC-2020) <jsn811@gmail.com>
2020-02-10 21:03:20 +0800

    gdgd

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,27 @@ const path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) 
 {
-  //res.render('index', { title: '首頁\n<a src="index.html">Title</a>' });
+  //
   res.sendFile( path.dirname(__dirname) + "/views/index.html" );
 });
 
 router.post('/loginAction', function (req, res) 
 {
-  res.send('POST request to the homepage');
-  // 输出 JSON 格式
-  var response = {
+  var response = 
+  {
     "uname":req.body.uname,
     "psw":req.body.psw
   };
-  console.log(response);
-  res.send(JSON.stringify(response));
+
+  console.log("用戶資訊：", response);
+  res.render('index', 
+    { 
+      title: '好棒，你登入了',
+      content: '使用的帳號是'+response.uname 
+    }
+  );
+
+
 })
 
 
