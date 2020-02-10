66608442aed4758f7c8995f3c07aeea12cd587f6
JCxYIS (DIYPC-2020) <jsn811@gmail.com>
2020-02-10 21:40:43 +0800

    dsadafarrgertgret

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', function(req, res, next)
   res.sendFile( path.dirname(__dirname) + "/views/index.html" );
 });
 
+// Log in
 router.post('/loginAction', function (req, res) 
 {
   var response = 
@@ -17,10 +18,30 @@ router.post('/loginAction', function (req, res)
     "psw":req.body.psw
   };
 
-  console.log("用戶資訊：", response);
-  res.render( 'index', {title: '好棒，你登入了',message: '使用的帳號是'+response.uname} );
+  console.log("[登入]用戶資訊：", response);
+  res.render( 'index', 
+  {
+    title: '好棒，你登入了', 
+    message: '使用的帳號是'+response.uname
+  } );
+})
 
+// Sign up
+router.post('/signupAction', function (req, res) 
+{
+  var response = 
+  {
+    "name":req.body.uname,
+    "uname":req.body.uname,
+    "psw":req.body.psw
+  };
 
+  console.log("[創號]用戶資訊：", response);
+  res.render( 'index', 
+  {
+    title: '創立帳號成功', 
+    message: '新帳號是'+response.uname
+  } );
 })
 
 
