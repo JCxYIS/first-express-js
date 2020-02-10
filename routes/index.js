bf3cb99f4c4415b9dfa65c981b47c96c31698d7b
JCxYIS (DIYPC-2020) <jsn811@gmail.com>
2020-02-10 20:37:52 +0800

    t

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: '首頁\n<a src="index.html">Title</a>' });
-  res.sendFile( __dirname + "..//views/index.html" );
+  res.sendFile("../views/index.html" );
 });
 
 
