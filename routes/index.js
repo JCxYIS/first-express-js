18935d3819c23494cd49794654283cf043c8f741
JCxYIS (DIYPC-2020) <jsn811@gmail.com>
2020-02-10 20:59:29 +0800

    post

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,23 @@ var router = express.Router();
 const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) 
+{
   //res.render('index', { title: '首頁\n<a src="index.html">Title</a>' });
   res.sendFile( path.dirname(__dirname) + "/views/index.html" );
 });
+
+router.post('/loginAction', function (req, res) 
+{
+  res.send('POST request to the homepage');
+  // 输出 JSON 格式
+  var response = {
+    "uname":req.body.uname,
+    "psw":req.body.psw
+  };
+  console.log(response);
+  res.send(JSON.stringify(response));
+})
 
 
 
