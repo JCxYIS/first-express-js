dbf03fc4d5578a801f9630b24bb9b08b65650fee
JCxYIS (DIYPC-2020) <jsn811@gmail.com>
2020-02-10 21:57:50 +0800

    kkk

--- routes/index.js
+++ routes/index.js
@@ -8,42 +8,4 @@ router.get('/', function(req, res, next)
   //
   res.sendFile( path.dirname(__dirname) + "/views/index.html" );
 });
-
-// Log in
-router.post('/loginAction', function (req, res) 
-{
-  var response = 
-  {
-    "uname":req.body.uname,
-    "psw":req.body.psw
-  };
-
-  console.log("[登入]用戶資訊：", response);
-  res.render( 'index', 
-  {
-    title: '好棒，你登入了', 
-    message: '使用的帳號是'+response.uname
-  } );
-})
-
-// Sign up
-router.post('/signupAction', function (req, res) 
-{
-  var response = 
-  {
-    "name":req.body.uname,
-    "uname":req.body.uname,
-    "psw":req.body.psw
-  };
-
-  console.log("[創號]用戶資訊：", response);
-  res.render( 'index', 
-  {
-    title: '創立帳號成功', 
-    message: '新帳號是'+response.uname
-  } );
-})
-
-
-
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+// Log in
+router.post('/loginAction', function (req, res) 
+{
+  var response = 
+  {
+    "uname":req.body.uname,
+    "psw":req.body.psw
+  };
+
+  console.log("[登入]用戶資訊：", response);
+  res.render( 'index', 
+  {
+    title: '好棒，你登入了', 
+    message: '使用的帳號是'+response.uname
+  } );
+})
+
+// Sign up
+router.post('/signupAction', function (req, res) 
+{
+  var response = 
+  {
+    "name":req.body.uname,
+    "uname":req.body.uname,
+    "psw":req.body.psw
+  };
+
+  console.log("[創號]用戶資訊：", response);
+  res.render( 'index', 
+  {
+    title: '創立帳號成功', 
+    message: '新帳號是'+response.uname
+  } );
+})
+
+
 
 module.exports = router;
